b4b228a77530abda9759d3684287555db0d7c24f
Amir Rehman Marwat <amirrehmanmwt@gmail.com>
2026-02-22 20:08:07 -0800

    components revised

--- revision1/src/App.jsx
+++ revision1/src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { Fragment } from 'react'
+import Submit from './components/submit'
+import Footer from './components/footer'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Fragment key={"helooo"}>
+      <h2>before</h2>
+      <Submit/>
+      <h2>after</h2>
+      <Footer />
+      </Fragment>
+    
+    
   )
 }
 
---
+++ revision1/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Submit from './submit'
+function Footer() {
+  return (
+    <>
+    <div>i am Footer</div>
+    <Submit />
+    </>
+  )
+}
+
+export default Footer
